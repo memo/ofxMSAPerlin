feb1831d3e732947bb323d88b7f3ec8890808c9e
Memo Akten <memo@memo.tv>
2019-02-01 04:52:08 +0300

    addon_config

---
+++ addon_config.mk
@@ -0,0 +1,11 @@
+meta:
+	ADDON_NAME = ofxMSAPerlin
+	ADDON_DESCRIPTION = Perlin Noise
+	ADDON_AUTHOR = Memo Akten, www.memo.tv
+	ADDON_TAGS = ""
+	ADDON_URL = https://github.com/memo/ofxMSAPerlin
+
+common:
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	ADDON_DEPENDENCIES =
